0d5d5236a29d3f29d26906bbd0af85c65056323b
boatx <>
2017-12-16 23:39:33 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Command Manager',
+      version='1.0',
+      description='Wrapper to argparser to simplify creating command line interfaces.',
+      author='BoatX',
+      url='https://github.com/boatx/command_manager',
+      packages=['command_manager'])
